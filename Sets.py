0dfad2f951589cf72ef197855c6bdd762cda8d07
David Peter Gomes <david.peter.gomes@gmail.com>
2025-07-13 12:22:43 +0530

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,14 @@
+S={}
+D=input("Enter your frieds name")
+L=input("Enter your favorate language")
+S.update({D:L})
+D=input("Enter your frieds name")
+L=input("Enter your favorate language")
+S.update({D:L})
+D=input("Enter your frieds name")
+L=input("Enter your favorate language")
+S.update({D:L})
+D=input("Enter your frieds name")
+L=input("Enter your favorate language")
+S.update({D:L})
+S
